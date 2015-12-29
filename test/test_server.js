1d88756466e08d1281e06ef1a2a3e38bfa8b3cd5
Cristian Molina <megatux@gmail.com>
2015-12-29 15:12:45 -0300

    Test any HTTP route

--- test/test_server.js
+++ test/test_server.js
@@ -3,10 +3,12 @@
 const request = require('superagent');
 const chai = require('chai');
 const expect = chai.expect;
+const loremIpsum = require('lorem-ipsum');
 const jq = require("chai-jq");
 chai.use(jq);
 
 const server = require('../server.js');
+const baseUrl = 'http://localhost:8080';
 
 describe('HTTP Server', function(){
   before(function () {
@@ -17,11 +19,29 @@ describe('HTTP Server', function(){
     server.close();
   });
 
-  it('should say hello', function(done){
-    request.get('http://localhost:8080/').end(function(err, res){
-      expect(res.status).to.equal(200);
-      expect(res.text).to.contain("Hola Node");
-      done();
-    });
-  })
+  describe('any route', function(){
+
+    it('should say hello on /', function(done){
+      request.get(baseUrl + '/').end(
+        function(err, res){
+          checkPageResult(err, res, "Hola Node");
+          done();
+        })
+      })
+
+    it('should say hello on a random route', function(done){
+      const word = loremIpsum({count: 1, units: 'words'})
+      request.get(baseUrl + '/' + word).end(
+        function(err, res){
+          checkPageResult(err, res, "Hola Node");
+          done();
+        })
+      })
+    })
 })
+
+function checkPageResult(err, res, content) {
+  expect(err).to.be.null;
+  expect(res.status).to.equal(200);
+  expect(res.text).to.contain(content);
+}
